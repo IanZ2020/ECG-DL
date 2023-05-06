93068b6bcef2eddca3ebcfd47476b825f06096e2
Ian Zhang <20307110123@fudan.edu.cn>
2023-05-06 23:08:27 +0800

    Modified data_utils.py

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -2,15 +2,15 @@ import wfdb     #导入wfdb包读取数据文件
 from IPython.display import display
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import os
 import scipy
 from scipy import signal
+from sklearn.model_selection import train_test_split
 
 AAMI=['N','V','L','R','A','F','S']
 
-# rootdir = 'data/european-st-t-database-1.0.0'
-rootdir = 'data/mit-bih-arrhythmia-database-1.0.0'
+rootdir = 'data/european-st-t-database-1.0.0'
+#rootdir = 'data/mit-bih-arrhythmia-database-1.0.0'
 # rootdir = 'data/mit-bih-st-change-database-1.0.0'
 # rootdir = 'data/sudden-cardiac-death-holter-database-1.0.0'        #心脏性猝死数据库
 
@@ -25,7 +25,7 @@ for file in files:
 
 for name in name_list:      # 遍历每一个人
     record = wfdb.rdrecord(rootdir+'/'+name)  # 读取一条记录（100），不用加扩展名
-    if 'MLII' in record.sig_name:       # 记录MLII导联的数据
+    if 'MLIII' in record.sig_name:       # 记录MLII导联的数据
         MLII.append(name)               # 记录下这个人
     annotation = wfdb.rdann(rootdir+'/'+name, 'atr')  # 读取一条记录的atr文件，扩展名atr
     for symbol in annotation.symbol:            # 记录下这个人所有的标记类型
@@ -53,10 +53,9 @@ for person in MLII:        # 读取导联方式为MLII的数据
     whole_signal=wfdb.rdrecord(rootdir + '/' + person).p_signal.transpose()     # 这个人的一整条数据
     lenth = len(whole_signal[0])
     while (k+1)*f*segmented_len<=lenth:    # 只要不到最后一组数据点
-        count+=1
         record = wfdb.rdrecord(rootdir + '/' + person, sampfrom=k * f * segmented_len,sampto=(k + 1) * f * segmented_len)  # 读取一条记录（100），不用加扩展名
         annotation = wfdb.rdann(rootdir + '/' + person, 'atr', sampfrom=k * f * segmented_len,sampto=(k + 1) * f * segmented_len)  # 读取一条记录的atr文件，扩展名atr
-        lead_index = record.sig_name.index('MLII')  # 找到MLII导联对应的索引
+        lead_index = record.sig_name.index('MLIII')  # 找到MLII导联对应的索引
         signal = record.p_signal.transpose()  # 两个导联，转置之后方便画图
         # segmented_data.append(signal[lead_index])   # 只记录MLII导联的数据段
         symbols=annotation.symbol
@@ -64,11 +63,6 @@ for person in MLII:        # 读取导联方式为MLII的数据
 
         re_signal = scipy.signal.resample(signal[lead_index], 3600)  # 采样
         re_signal_3 = np.round(re_signal, 3)
-        print('resignal', re_signal_3)
-        segmented_data.append(re_signal_3)
-
-        print('symbols', symbols, len(symbols))
-
         if len(symbols) == 0:
             k+=1
             continue
@@ -86,15 +80,22 @@ for person in MLII:        # 读取导联方式为MLII的数据
             continue
         if(label != 'N'):
             abnormal += 1
+        
+        count+=1
+        print('resignal', re_signal_3)
+        print('symbols', symbols, len(symbols))
+        segmented_data.append(re_signal_3)
         segmented_label.append(label)
         print(label + ' No. '+ str(count) +' No. of abnormal cases= '+ str(abnormal))
         k+=1
 print('begin to save dataset!')
 
+X_train, X_test, Y_train, Y_test = train_test_split(segmented_data, segmented_label, test_size=0.25, shuffle=True)
+train_df = pd.DataFrame(X_train)
+train_df['Y'] = Y_train
+train_df.to_csv('train.csv', index=False)
 
-segmented_data=pd.DataFrame(segmented_data)
-segmented_label=pd.DataFrame(segmented_label)
-segmented_data.to_csv('X_mitsh_MLII.csv', index=False)
-segmented_label.to_csv('Y_mitsh_MLII.csv', index=False)
-
+test_df = pd.DataFrame(X_test)
+test_df['Y'] = Y_test
+test_df.to_csv('test.csv', index=False)
 print('Finished!')
